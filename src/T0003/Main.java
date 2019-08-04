286a1fba7878f8c9e4be3f88fc0eb724a69b5e08
小桀 <812797569@qq.com>
2019-08-04 10:06:57 +0800

    T3 T79

--- src/T0003/Main.java
+++ src/T0003/Main.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(new Solution2().lengthOfLongestSubstring("abcabcbb"));
     }
 }
 
@@ -13,32 +13,59 @@ class Solution {
         int[] map = new int[128];
 
         for (int i = 0; i < map.length; i++) {
-            map[i]=-1;
+            map[i] = -1;
         }
 
         int i = 0;
         int count = 0;
         int max = Integer.MIN_VALUE;
-        while(i < s.length()){
-            if(map[s.charAt(i)]==-1){
+        while (i < s.length()) {
+            if (map[s.charAt(i)] == -1) {
                 count++;
-                map[s.charAt(i)]=i;
+                map[s.charAt(i)] = i;
                 i++;
-            }else {
-                if(count>max){
+            } else {
+                if (count > max) {
                     max = count;
                 }
 
-                count=0;
-                i=map[s.charAt(i)]+1;
+                count = 0;
+                i = map[s.charAt(i)] + 1;
                 for (int j = 0; j < map.length; j++) {
-                    map[j]=-1;
+                    map[j] = -1;
                 }
             }
         }
-        if(count>max){
+        if (count > max) {
             max = count;
         }
+        return max;
+    }
+}
+
+/**
+ * 使用滑动窗口解题
+ */
+class Solution2 {
+    public int lengthOfLongestSubstring(String s) {
+        int[] map = new int[128];
+
+        int low = 0, high = 0;
+        int max = Integer.MIN_VALUE;
+        while (high < s.length()) {
+            if (map[s.charAt(high)] == 0) {
+                map[s.charAt(high)] = 1;
+                high++;
+            } else {
+                max = Math.max(max, high - low);
+
+                while (low < high && s.charAt(low) != s.charAt(high)) {
+                    map[s.charAt(low++)] = 0;
+                }
+                map[s.charAt(low++)] = 0;
+            }
+        }
+        max = Math.max(max, high - low);
         return max;
     }
 }
